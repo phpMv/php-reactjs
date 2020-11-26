1cec2ee2b0efeab84addde7b46dcabddc198f692
jcheron <myaddressmail@gmail.com>
2020-11-26 18:02:42 +0100

    add http server + www + selenium

---
+++ tests/www/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset='UTF-8'>
+	<title>React tests</title>
+	<script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+	<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+</head>
+<body>
+Hello React !
+</body>
+<?php
+?>
+</html>
