93c425ef6793539da24e2e63fec601dda41bb9a4
jcheron <myaddressmail@gmail.com>
2020-11-27 00:45:33 +0100

    Update ReactJSTest.php

--- tests/unit/ReactJSTest.php
+++ tests/unit/ReactJSTest.php
@@ -2,38 +2,40 @@
 use PHPMV\react\ReactJS;
 use function PHPUnit\Framework\assertEquals;
 
-/**
- * ReactJS test case.
- */
-class ReactJSTest extends \Codeception\Test\Unit {
+if (! class_exists(ReactJSTest::class)) {
 
 	/**
-	 *
-	 * @var ReactJS
+	 * ReactJS test case.
 	 */
-	protected $react;
+	class ReactJSTest extends \Codeception\Test\Unit {
 
-	protected function _before() {
-		$this->react = new ReactJS();
-	}
+		/**
+		 *
+		 * @var ReactJS
+		 */
+		protected $react;
 
-	protected function _after() {
-		$this->react = null;
-	}
+		protected function _before() {
+			$this->react = new ReactJS();
+		}
 
-	protected function assertEqualsIgnoreNewLines($expected, $actual) {
-		$this->assertEquals(trim(preg_replace('/\R+/', '', $expected)), trim(preg_replace('/\R+/', '', $actual)));
-	}
+		protected function _after() {
+			$this->react = null;
+		}
 
-	/**
-	 * Tests ReactJS::createComponent()
-	 */
-	public function testCreateComponent() {
-		$this->assertEquals('', $this->react->compile());
-		$compo = $this->react->createComponent('MyCompo');
-		$compo->addConstructor('console.log("super");');
-		$compo->addMethod('method', 'alert(a);alert(b);', 'a', 'b');
-		$compo->addRender('<div>
+		protected function assertEqualsIgnoreNewLines($expected, $actual) {
+			$this->assertEquals(trim(preg_replace('/\R+/', '', $expected)), trim(preg_replace('/\R+/', '', $actual)));
+		}
+
+		/**
+		 * Tests ReactJS::createComponent()
+		 */
+		public function testCreateComponent() {
+			$this->assertEquals('', $this->react->compile());
+			$compo = $this->react->createComponent('MyCompo');
+			$compo->addConstructor('console.log("super");');
+			$compo->addMethod('method', 'alert(a);alert(b);', 'a', 'b');
+			$compo->addRender('<div>
         <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
@@ -52,7 +54,7 @@ class ReactJSTest extends \Codeception\Test\Unit {
           {this.renderSquare(8)}
         </div>
       </div>');
-		$this->assertEqualsIgnoreNewLines('<script>class MyCompo extends React.Component {
+			$this->assertEqualsIgnoreNewLines('<script>class MyCompo extends React.Component {
 constructor(props){
 	super(props);
 console.log("super");
@@ -74,8 +76,8 @@ render(){
 },this.renderSquare(6),this.renderSquare(7),this.renderSquare(8)));
 }
 }</script>', $this->react->compile());
-		$this->react->renderComponent("<MyCompo/>", "#root");
-		$this->assertEqualsIgnoreNewLines('<script>class MyCompo extends React.Component {
+			$this->react->renderComponent("<MyCompo/>", "#root");
+			$this->assertEqualsIgnoreNewLines('<script>class MyCompo extends React.Component {
 constructor(props){
 	super(props);
 console.log("super");
@@ -98,17 +100,17 @@ render(){
 }
 }const domContainer = document.querySelector("#root");
 ReactDOM.render(React.createElement(MyCompo,[]), domContainer);</script>', $this->react->compile());
-	}
+		}
 
-	/**
-	 * Tests ReactJS::compile()
-	 */
-	public function testCompile() {
-		$this->assertEquals('', $this->react->compile());
-		$this->react->renderComponent("<button />", "#root");
+		/**
+		 * Tests ReactJS::compile()
+		 */
+		public function testCompile() {
+			$this->assertEquals('', $this->react->compile());
+			$this->react->renderComponent("<button />", "#root");
 
-		$this->assertEqualsIgnoreNewLines('<script>const domContainer = document.querySelector("#root");
+			$this->assertEqualsIgnoreNewLines('<script>const domContainer = document.querySelector("#root");
 ReactDOM.render(React.createElement("button",[]), domContainer);</script>', $this->react->compile());
+		}
 	}
 }
-
