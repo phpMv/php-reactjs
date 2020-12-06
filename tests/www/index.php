ed6ef2fd0752c991d7b82ddc04a00b837ef4b640
jcheron <myaddressmail@gmail.com>
2020-12-06 01:57:11 +0100

    update www index

--- tests/www/index.php
+++ tests/www/index.php
@@ -1,27 +1,34 @@
 <?php
-define('ROOT', __DIR__ . DIRECTORY_SEPARATOR);
-require ROOT . 'vendor/autoload.php';
 include 'c3.php';
-?>
-<!doctype html>
-<html>
-<head>
-	<meta charset='UTF-8'>
-	<title>React tests</title>
-	<script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
-	<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
-</head>
-<body>
-<div id='react'>
-</div>
-<?php
-$inc = $_GET['c'] ?? '';
-if ($inc != '') {
-	include ROOT . 'include/' . $inc . '.php';
-} else {
-	echo "Hello React !";
-}
-?>
-</body>
+define('ROOT', __DIR__ . DIRECTORY_SEPARATOR);
+define('MY_APP_STARTED', true);
 
-</html>
+require ROOT . 'vendor/autoload.php';
+if (\substr($_GET['c'], 0, 2) === 'c3') {
+	require 'c3.php';
+} else {
+	?>
+	<!doctype html>
+	<html>
+	<head>
+		<meta charset='UTF-8'>
+		<title>React tests</title>
+		<script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+		<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+	</head>
+	<body>
+	<div id='react'>
+	</div>
+	<?php
+	$inc = $_GET['c'] ?? '';
+	if ($inc != '') {
+		include ROOT . 'include/' . $inc . '.php';
+	} else {
+		echo "Hello React !";
+	}
+	?>
+	</body>
+
+	</html>
+	<?php
+}
